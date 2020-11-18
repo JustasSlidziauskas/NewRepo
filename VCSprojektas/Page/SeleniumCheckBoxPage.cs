c6a6dfa9c31a21cc95fb947df0b46bd2202afa2e
JustasSlidziauskas <j.slidziauskas19@gmail.com>
2020-11-18 16:19:21 +0200

    Pašalinta BasePage paveldimumo funkcija

---
+++ VCSprojektas/Page/SeleniumCheckBoxPage.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSprojektas.Page
+{
+    public class SeleniumCheckBoxPage 
+    {
+
+        private static IWebDriver _driver;
+        private IWebElement checkboxclick => _driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement checkBoxResult => _driver.FindElement(By.Id("txtAge"));
+
+
+        public SeleniumCheckBoxPage(IWebDriver webDriver)
+        {
+            _driver = webDriver;
+        }
+
+        public void ClickCheckBox()
+        {
+            checkboxclick.Click();           
+        }
+        private void WaitForResultBox()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => checkBoxResult.Displayed);
+        }
+
+        public void CheckResult(string expectedResult)
+        {
+            WaitForResultBox();
+            Assert.IsTrue(checkBoxResult.Text.Contains(expectedResult), $"Result is not the same, expented {expectedResult}, but was {checkBoxResult.Text}");
+        }
+
+    }
+}
---
+++ VCSprojektas/Test/SeleniumCheckBoxTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VCSprojektas.Page;
+
+namespace VCSprojektas.Test
+{
+    public class SeleniumCheckBoxTest
+    {
+        private static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Manage().Window.Maximize();
+            //_driver.FindElement(By.Id("cookiescript_reject")).Click(); jeigu bus cookies 
+        }
+
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void TestSeleniumInputFirstCechkBox()
+        {
+            SeleniumCheckBoxPage page = new SeleniumCheckBoxPage(_driver);
+
+            page.ClickCheckBox();
+            page.CheckResult("checked");
+        }
+
+
+
+
+
+
+
+
+    }
+}
